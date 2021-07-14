2682b4a5e7c7ab8bab3e13f9b6f9e2316ad936a2
Eduardo Lima <dualmeidalima@gmail.com>
2021-07-13 21:52:01 -0300

    Section 3: Navigation (Links and Programmatically) and 404

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => (
+    <div>
+        <h1>NOTHING WAS EVER FOUND</h1>
+    </div>
+)
+
+export default NotFound
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,46 @@
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 import '../styles/globals.css'
+import { useRouter } from 'next/router'
 
 // This component is a wrapper for all components
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    const router = useRouter()
+
+    const navigateHandler = () => {
+        router.push('/')
+        // router.push({
+        //     pathname: '/person/[id]/[resource]',
+        //     query: { id: 1, resource: 'contacts' },
+        // })
+    }
+
+    return (
+        <div className='container'>
+            <nav className='navbar' role='navigation' aria-label='main navigation'>
+                <div className='navbar-menu'>
+                    <Link href='/blog/2020/somevalue'>
+                        <a className='navbar-item'>Blog</a>
+                    </Link>
+                    <Link href='/items'>
+                        <a className='navbar-item'>Item</a>
+                    </Link>
+                    <Link href='/person'>
+                        <a className='navbar-item'>Person</a>
+                    </Link>
+                    <Link href='/about'>
+                        <a className='navbar-item'>About</a>
+                    </Link>
+                    <div className='navbar-end'>
+                        <div className='navbar-item'>
+                            <button className='button' onClick={navigateHandler}>Go Programmatically</button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <Component {...pageProps} />
+        </div>
+    )
 }
 
 export default MyApp
